e11b0cf386f5ec952894fd412b441a033c313ceb
scott63a <scott_dabill2001@yahoo.com>
2021-02-02 19:52:36 -0500

    Create deafGrandma.js

---
+++ deafGrandma.js
@@ -0,0 +1,21 @@
+let counter = 0;
+  function deafGrandma(input){
+      if (input === ''){
+        output = 'WHAT?!';
+      } else if (/[a-z]/.test(input)){
+        output = 'SPEAK UP, KID!';
+      } else if (input === 'GOODBYE!' && counter == 0){
+          output = 'LEAVING SO SOON?';
+          counter++;
+      } else if (input === 'GOODBYE!' && counter == 1){
+          output = 'LATER SKATER';
+          counter++;   
+      } else if (input === input.toUpperCase()){
+        output = 'NO, NOT SINCE 1946!';
+      }
+      console.log(output);
+      };
+    
+deafGrandma('HI GRANDMA');
+deafGrandma('GOODBYE!');
+deafGrandma('GOODBYE!');
