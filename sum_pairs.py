38d9f5ade9e9d0a8751313a201ffcab2273f1708
scott63a <scott_dabill2001@yahoo.com>
2021-02-04 09:59:02 -0500

    Update sum_pairs.py

--- sum_pairs.py
+++ sum_pairs.py
@@ -1,9 +1,9 @@
 pairs = []
 def sum_pairs(array, sum):
   for i in range(len(array)-1):
-        for j in range(i+1, len(array)):
-            if array[i] + array[j] == sum:
-                pairs.append([array[i], array[j]])
+        for k in range(i+1, len(array)):
+            if array[i] + array[k] == sum:
+                pairs.append([array[i], array[k]])
             else:
                 print('unable to find pairs')
   print(pairs)
